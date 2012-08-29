31a7949e681af93e15d82f17c450079b1f6461d8
Tania <tanzjacob@gmail.com>
2012-08-29 23:57:46 +1200

    Changed preview page's title, tried implementing scroll view but its not working also got a selection over a button with a colour possible

--- CowsEyeAndroidApplication/src/nz/co/android/cowseye/activity/PreviewActivity.java
+++ CowsEyeAndroidApplication/src/nz/co/android/cowseye/activity/PreviewActivity.java
@@ -16,6 +16,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -85,6 +86,7 @@ public class PreviewActivity extends AbstractSubmissionActivity {
 		//		location.setOnClickListener(new Utils.StartNextActivityEventOnClickListener(this, RecordLocationActivity.class));
 
 		description = (TextView)findViewById(R.id.PreviewDescriptionText);
+		description.setMovementMethod(new ScrollingMovementMethod());
 		String descriptionText = submissionEventBuilder.getImageDescription();
 		if (descriptionText.length() > maxLength) descriptionText = descriptionText.substring(0, maxLength);
 		description.setText(submissionEventBuilder.getImageDescription());
