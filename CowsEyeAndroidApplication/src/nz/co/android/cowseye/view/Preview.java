16e88e4a3093dd02fa50982f450c1d176fa7014c
Mitch <mitchell.lane52@gmail.com>
2012-08-29 15:33:09 +1200

    Changest to preview

--- CowsEyeAndroidApplication/src/nz/co/android/cowseye/view/Preview.java
+++ CowsEyeAndroidApplication/src/nz/co/android/cowseye/view/Preview.java
@@ -184,7 +184,7 @@ public class Preview extends SurfaceView implements SurfaceHolder.Callback {
 		}
 //		Log.d(toString(), "resizing frame b4");
 		//if measured then don't resize frame
-		if(!resizedFrame && horizontal){
+		if(!resizedFrame){
 //			Log.d(toString(), "resizing frame after");
 			resizedFrame = true;
 			//set parents dimensions to keep aspect ratio of 4:4
